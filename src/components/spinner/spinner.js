2d425284d64929501053a0bafb7c28356d4cd26e
msp196811 <44489596+msp196811@users.noreply.github.com>
2020-11-11 07:29:39 +0300

    Add files via upload

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './spiner.css'
+const Spiner = () => {
+    return (
+        <div className="section__spinner">
+            <div className="spin"></div>
+        </div>
+    )
+}
+export default Spiner;
